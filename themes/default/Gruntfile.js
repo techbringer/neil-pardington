952930454f57a421dc0dca65fcec95bc28e501dd
Tech Bringer <leochenftw@gmail.com>
2016-07-24 20:13:55 -1200

    updated Gruntfile.js

--- themes/default/Gruntfile.js
+++ themes/default/Gruntfile.js
@@ -12,31 +12,6 @@ module.exports = function(grunt) {
 			}
 		},
 		
-		requirejs: {
-			compile: {
-				options: {
-					baseUrl: 'js',
-					dir: 'build',
-					keepBuildDir: true,
-					mainConfigFile: 'js/config.js',
-					fileExclusionRegExp: /^\.|css|scss|fonts|img|templates|build|\.(jpg|jpeg|gif|css|pdf|png|php|html|ico|md|json)+$|readme|r.js|app.build.js|config.js|\.svn|docs|examples|test/,
-					optimizeCss: 'none',
-	
-					modules: [{"name":"pagetypes\/blogentry"},{"name":"pagetypes\/blogholder"},{"name":"pagetypes\/default"},{"name":"pagetypes\/newsarticle"},{"name":"pagetypes\/newsholder"},{"name":"pagetypes\/page"},{"name":"pagetypes\/userdefinedform"}] // add modules here. e.g. : {name: 'pagetypes/pagename'}
-				}
-			}
-		},
-		
-		/*
-imageoptim: {
-			files: ['img'],
-			options: {
-				jpegMini: false,
-				imageAlpha: true,
-				quitAfter: false
-			}
-		},
-*/
 		
 		watch: {
 			css: {
